be75bfcdbbbb1d04aec1c4511fcdcfbd3daa1cfd
angelYorks <angelyorks2003@gmail.com>
2024-12-30 01:41:05 -0500

    Funcionalidad con division entre logica e interfaz aplicacion de UI simple

--- app/src/main/java/com/example/buscaminas/interfaz/MainActivity.kt
+++ app/src/main/java/com/example/buscaminas/interfaz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.buscaminas.interfaz
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewTreeObserver
@@ -10,11 +11,12 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.buscaminas.R
 import com.example.buscaminas.logica.Celda
-import com.example.buscaminas.logica.CeldaReveladaListener
+import com.example.buscaminas.logica.Game
 import com.example.buscaminas.logica.Tablero
 import kotlin.math.min
 
@@ -23,11 +25,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var minasRestantes: TextView
     private lateinit var tableroGrid: GridLayout
     private lateinit var reiniciarButton: AppCompatButton
-    lateinit var tablero: Tablero
+    private lateinit var game: Game
 
-    val filas: Int = 14
-    val columnas: Int = 8
-    val minas : Int = 5
+    private val filas: Int = 14
+    private val columnas: Int = 8
+    private val minas: Int = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +41,11 @@ class MainActivity : AppCompatActivity() {
         reiniciarButton = findViewById(R.id.reiniciarButton)
 
         reiniciarButton.setOnClickListener {
-            reiniciarJuego(filas,columnas,minas)
+            reiniciarJuego()
         }
 
-        reiniciarJuego(filas,columnas, minas)
+        game = Game(filas, columnas, minas)
+        reiniciarJuego()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -51,82 +54,65 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun reiniciarJuego(filas: Int, columnas: Int, minas: Int) {
-        // Inicializa el tablero y limpia el GridLayout
-        tablero = Tablero(filas, columnas, minas)
-        tablero.reiniciarTablero()
+    private fun reiniciarJuego() {
+        game.reiniciarJuego()
         tableroGrid.removeAllViews()
         minasRestantes.text = "Minas restantes: $minas"
         Log.d("Minas", minas.toString())
-        // Establece el número de filas y columnas en el GridLayout
         tableroGrid.rowCount = filas
         tableroGrid.columnCount = columnas
 
-        // Crea un listener para calcular el tamaño de los botones dinámicamente
         val listener = object : ViewTreeObserver.OnGlobalLayoutListener {
             override fun onGlobalLayout() {
-                // Calcula el tamaño disponible del GridLayout
                 val gridWidth = tableroGrid.width
                 val gridHeight = tableroGrid.height
-
-                // Calcula el tamaño máximo para que los botones sean cuadrados y encajen en el GridLayout
                 val buttonSize = Math.min(gridWidth / columnas, gridHeight / filas)
 
-                // Agrega los botones al GridLayout
                 for (fila in 0 until filas) {
                     for (columna in 0 until columnas) {
                         val botonCelda = Button(this@MainActivity)
-
-                        // Configura cada botón con el tamaño calculado
                         configurarBotonInicial(botonCelda, fila, columna, buttonSize)
-                        revelarBotonCelda(botonCelda, fila, columna,filas, columnas,minas)
-                        // Agrega el botón al GridLayout
+                        revelarBotonCelda(botonCelda, fila, columna)
                         tableroGrid.addView(botonCelda)
                     }
                 }
 
-                // Elimina el listener para evitar llamadas repetidas
                 tableroGrid.viewTreeObserver.removeOnGlobalLayoutListener(this)
             }
         }
 
-        // Agrega el listener al GridLayout
         tableroGrid.viewTreeObserver.addOnGlobalLayoutListener(listener)
     }
 
-    fun configurarBotonInicial(botonCelda: Button, fila: Int, columna: Int, buttonSize: Int) {
+    private fun configurarBotonInicial(botonCelda: Button, fila: Int, columna: Int, buttonSize: Int) {
         botonCelda.layoutParams = GridLayout.LayoutParams().apply {
             width = buttonSize
             height = buttonSize
             columnSpec = GridLayout.spec(columna)
             rowSpec = GridLayout.spec(fila)
-            setMargins(0, 0, 0, 0) // Espaciado entre botones
-
+            setMargins(0, 0, 0, 0)
         }
         botonCelda.setPadding(0, 0, 0, 0)
         botonCelda.text = ""
-        botonCelda.setBackgroundColor(resources.getColor(R.color.white)) // Color inicial del botón
+        botonCelda.setTextColor(Color.parseColor("#37474F"))
+        botonCelda.setBackgroundResource(R.drawable.selector_fondo_boton) // Fondo personalizado
     }
 
-
-    fun revelarBotonCelda(button: Button, fila: Int, columna: Int, filas: Int, columnas: Int, minas: Int) {
-        val celda = tablero.tablero[fila][columna]
+    private fun revelarBotonCelda(button: Button, fila: Int, columna: Int) {
+        val celda = game.tablero.tablero[fila][columna]
 
         button.setOnClickListener {
             if (!celda.descubierto && !celda.marcado) {
-                val revelado = tablero.revelarCelda(fila, columna)
+                val revelado = game.revelarCelda(fila, columna)
                 if (revelado) {
-                    actualizarTablero(filas, columnas, minas)
-                    marcadorMina(minas)  // Actualiza las minas restantes después de revelar una celda
-                    // Verificar si el jugador ha perdido
-                    if (perdisteGame(filas, columnas)) {
-                        // Muestra mensaje o realiza alguna acción si el jugador ha perdido
+                    actualizarTablero()
+                    marcadorMina()
+                    if (game.perdisteGame()) {
+                        game.reiniciarJuego()
                         mostrarMensaje("¡Perdiste!")
                     }
-
-                    // Verificar si el jugador ha ganado
-                    if (ganasteGame(filas, columnas, minas)) {
-                        // Muestra mensaje o realiza alguna acción si el jugador ha ganado
+                    if (game.ganasteGame()) {
+                        game.reiniciarJuego()
                         mostrarMensaje("¡Ganaste!")
                     }
                 }
@@ -134,40 +120,35 @@ class MainActivity : AppCompatActivity() {
         }
 
         button.setOnLongClickListener {
-            // Cambiar el estado del botón a "banderita"
             if (button.text == "🚩") {
                 celda.marcado = false
-                button.text = "" // Quitar la banderita
+                button.text = ""
             } else {
                 celda.marcado = true
-                button.text = "🚩" // Poner la banderita
+                button.text = "🚩"
+                if (game.ganasteGame()) {
+                    game.reiniciarJuego()
+                    mostrarMensaje("¡Ganaste!")
+                }
             }
-
-            // Actualizar las minas restantes cada vez que se marque o desmarque una celda
-            marcadorMina(minas)
-            Log.d("Marcados ",tablero.contarCeldasMarcadas().toString())
-
-            true // Indica que el evento ha sido consumido
+            marcadorMina()
+            true
         }
     }
 
-    fun actualizarTablero(filas: Int, columnas: Int, minas:Int) {
-
-
+    private fun actualizarTablero() {
         for (fila in 0 until filas) {
             for (columna in 0 until columnas) {
                 val boton = tableroGrid.getChildAt(fila * columnas + columna) as Button
-                val celda = tablero.tablero[fila][columna]
+                val celda = game.tablero.tablero[fila][columna]
 
                 if (celda.descubierto && !celda.marcado) {
                     configuracionBotonPresionado(boton, celda)
 
-                    if (celda.minasVecinas == 0 && !celda.esMina && !celda.marcado) {
-                        boton.text = ""  // Espacio vacío si no hay minas vecinas
-                    } else if (celda.esMina) {
-                        boton.text = "*"  // Muestra el símbolo de mina
-                    } else {
-                        boton.text = celda.minasVecinas.toString()  // Número de minas vecinas
+                    boton.text = when {
+                        celda.minasVecinas == 0 && !celda.esMina && !celda.marcado -> ""
+                        celda.esMina -> "*"
+                        else -> celda.minasVecinas.toString()
                     }
 
                     boton.invalidate()
@@ -176,62 +157,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun marcadorMina(minas: Int) {
-        // Contar las minas marcadas, no solo las celdas marcadas
-        val minasMarcadas = tablero.contarCeldasMarcadas()
-
-        // Mostrar el número de minas restantes
+    private fun marcadorMina() {
+        val minasMarcadas = game.contarCeldasMarcadas()
         minasRestantes.text = "Minas restantes: ${minas - minasMarcadas}"
     }
 
-    fun configuracionBotonPresionado(boton: Button, celda: Celda) {
-
-        if (!celda.marcado){
-            boton.isEnabled = false // Deshabilitar el botón después de revelarlo
-        }else{
-            boton.isEnabled = true
-        }
-
-        // Cambiar el color de fondo dependiendo de si es mina o no
+    private fun configuracionBotonPresionado(boton: Button, celda: Celda) {
+        boton.isEnabled = !celda.marcado
         if (celda.esMina) {
-            boton.setTextColor(resources.getColor(R.color.teal_700, null))
-            boton.setBackgroundColor(resources.getColor(R.color.purple_200, null))
+            boton.setBackgroundResource(R.drawable.button_pressed_background)
         } else {
-            boton.setBackgroundColor(resources.getColor(R.color.teal_200, null))
+            boton.setBackgroundResource(R.drawable.button_pressed_background)
         }
 
         boton.invalidate()
     }
 
-    fun perdisteGame(filas:Int, columnas:Int):Boolean{
-        for (fila in 0 until filas){
-            for (columna in 0 until columnas){
-                val celda = tablero.tablero[fila][columna]
 
-                if(celda.descubierto && celda.esMina){
-                    Log.d("ESTADO: ", "PERDISTE")
-                    return true
-                }
-            }
-        }
-        return false
-    }
-
-    fun ganasteGame(filas:Int, columnas:Int, minas:Int):Boolean{
-        var desactivados: Int =0
-        for (fila in 0 until filas){
-            for (columna in 0 until columnas){
-                val celda = tablero.tablero[fila][columna]
-
-                if(celda.marcado && celda.esMina && (tablero.celdasReveladas() + tablero.contarCeldasMarcadas()== filas*columnas)){
-                    desactivados++
-                }
-            }
-        }
-        return desactivados == minas
-    }
-    fun mostrarMensaje(mensaje: String) {
+    private fun mostrarMensaje(mensaje: String) {
         Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
-
     }
 }
--- app/src/main/java/com/example/buscaminas/logica/Game.kt
+++ app/src/main/java/com/example/buscaminas/logica/Game.kt
@@ -1,19 +1,57 @@
 package com.example.buscaminas.logica
 
-class Game(val filas:Int, val columnas: Int, val numeroMinas: Int) {
+import android.util.Log
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.Toast
+import com.example.buscaminas.R
 
-    private val tablero: Tablero = Tablero(filas, columnas, numeroMinas)
 
-    fun revelarCelda(fila: Int, columna: Int):Boolean{
-        return tablero.revelarCelda(filas, columna)
-    }
+class Game(private val filas: Int, private val columnas: Int, private val minas: Int) {
 
-    fun verificarVictoria(): Boolean{
-        return tablero.verificarVictoria()
-    }
+    val tablero: Tablero = Tablero(filas, columnas, minas)
 
-    fun reiniciarTablero(){
+    fun reiniciarJuego() {
         tablero.reiniciarTablero()
     }
 
+    fun revelarCelda(fila: Int, columna: Int): Boolean {
+        return tablero.revelarCelda(fila, columna)
+    }
+
+    fun contarCeldasMarcadas(): Int {
+        return tablero.contarCeldasMarcadas()
+    }
+
+    fun celdasReveladas(): Int {
+        return tablero.celdasReveladas()
+    }
+
+    fun perdisteGame(): Boolean {
+        for (fila in 0 until filas) {
+            for (columna in 0 until columnas) {
+                val celda = tablero.tablero[fila][columna]
+                if (celda.descubierto && celda.esMina) {
+                    Log.d("ESTADO: ", "PERDISTE")
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun ganasteGame(): Boolean {
+        var desactivados = 0
+        for (fila in 0 until filas) {
+            for (columna in 0 until columnas) {
+                val celda = tablero.tablero[fila][columna]
+                if (celda.marcado && celda.esMina &&
+                    (tablero.celdasReveladas() + tablero.contarCeldasMarcadas() == filas * columnas)
+                ) {
+                    desactivados++
+                }
+            }
+        }
+        return desactivados == minas
+    }
 }
